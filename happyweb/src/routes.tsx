161e7bca5cfd751db245c996666b17d2bce931ee
Deimos177 <brucevieira.2009@gmail.com>
2020-10-13 12:07:30 -0300

    front-end initial

---
+++ happyweb/src/routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route, Router} from 'react-router-dom'
+
+import Landing from './pages/Landing'
+import OrphanagesMap from './pages/OrphanagesMap'
+
+function Routes(){
+    return(
+        <BrowserRouter>
+        <switch>
+          <Route path="/" exact component={Landing}/>
+          <Route path="/app/" component={OrphanagesMap}/>
+        </switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes
