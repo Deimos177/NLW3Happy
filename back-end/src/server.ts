4f4069a1c6ec384432f0cff910a94f67948f26fa
Deimos177 <brucevieira.2009@gmail.com>
2020-10-15 01:13:04 -0300

    back-end finished

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import './database/connection'
 import path from 'path'
 import 'express-async-errors'
+import cors from 'cors'
 
 import routes from './routes'
 import errorHandler from './errors/handler'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
