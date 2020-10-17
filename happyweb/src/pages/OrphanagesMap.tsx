ca7d856e323005fda89247d0715fe44cb5de5858
Deimos177 <brucevieira.2009@gmail.com>
2020-10-16 22:50:52 -0300

    web front-end 'finished'

--- happyweb/src/pages/OrphanagesMap.tsx
+++ happyweb/src/pages/OrphanagesMap.tsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
-import {FiPlus} from 'react-icons/fi'
-import { Map, TileLayer } from 'react-leaflet'
-
-import 'leaflet/dist/leaflet.css'
+import {FiPlus, FiArrowRight, FiArrowLeft} from 'react-icons/fi'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import { useHistory } from 'react-router-dom';
+import api from '../services/api';
 
 import mapMarkerImg from '../images/map-marker.svg'
+import mapIcon from '../utils/mapIcon'
 
 import '../styles/pages/orphanages-map.css'
 
+interface Orphange{
+  id: number,
+  latitude: number,
+  longitude: number,
+  name: string
+}
+
 function OrphanagesMap(){
+  //state devolve o valor e uma função para alterar o valor
+  const [orphanages, setOrphanages] = useState<Orphange[]>([])
+
+  //use state cria um ciclo de renderização, fazendo com que qualquer alteração reflita na tela.
+
+  useEffect(()=> {
+    api.get('orphanages').then(response => {
+      setOrphanages(response.data)
+    })
+  }, [])
+
+  const {goBack} = useHistory()
     return(
       <div id="page-map">
         <aside>
@@ -23,11 +43,14 @@ function OrphanagesMap(){
           <footer>
             <strong>São Paulo</strong>
             <span>São Paulo</span>
+            <button type="button" onClick={goBack}>
+            <FiArrowLeft size={24} color="#FFF" />
+            </button>
           </footer>
         </aside>
 
         <Map 
-        center={[-23.7642516,-46.6801928]}
+        center={[-23.5680982,-46.6700479]}
         zoom={15}
         style={{width: '100%', height: '100%'}}
         >
@@ -35,9 +58,25 @@ function OrphanagesMap(){
             <TileLayer 
             url={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
             />
+
+            {orphanages.map(orphanage => {
+              return(
+                <Marker
+                  key={orphanage.id} 
+                  icon={mapIcon}
+                  position={[orphanage.latitude,orphanage.longitude]}>
+                  <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                    {orphanage.name}
+                    <Link to={`orphanages/${orphanage.id}`}>
+                      <FiArrowRight size={20} color="FFF"/>
+                    </Link>
+                  </Popup>
+                </Marker>
+              )
+            })}
         </Map>
 
-        <Link to="" className="create-orphanage">
+        <Link to="/orphanages/create" className="create-orphanage">
             <FiPlus size={32} color="#fff"/>
         </Link>
       </div>
--- happyweb/src/routes.tsx
+++ happyweb/src/routes.tsx
@@ -3,6 +3,8 @@ import {BrowserRouter, Switch, Route, Router} from 'react-router-dom'
 
 import Landing from './pages/Landing'
 import OrphanagesMap from './pages/OrphanagesMap'
+import orphanage from './pages/Orphanage'
+import createOrphanage from './pages/CreateOrphanage'
 
 function Routes(){
     return(
@@ -10,6 +12,8 @@ function Routes(){
         <switch>
           <Route path="/" exact component={Landing}/>
           <Route path="/app/" component={OrphanagesMap}/>
+          <Route path="/orphanages/create" component={createOrphanage}/>
+          <Route path="/orphanages/:id" component={orphanage}/>
         </switch>
         </BrowserRouter>
     )
